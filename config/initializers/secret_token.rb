b7add4f784d93f054885d65de8776d00f0164eba
sharonsteed <sharon.steed@gmail.com>
2013-06-17 15:36:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharonsBlog::Application.config.secret_token = 'cca53092c3c6cc1e5a3f7f3f3a092b8df96572838c7a69aa40f5a60b31cd6775677416a2ba188f59ec68ae3c9cc5f973b6d04940e8a4648cfc5355b4409d355f'
